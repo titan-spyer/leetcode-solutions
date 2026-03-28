a3562cfb659bf992b9d1caba623b5cbd48911938
titan-spyer <mr.satya02@gmail.com>
2026-03-28 20:24:56 +0530

    feat: add another problem related to Linkedlist

---
+++ LinkedList/LC2095.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head.next:
+            return None
+        # dummy = ListNode(0, head)
+        # slow = dummy
+        slow = head
+        fast = head.next.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        slow.next = slow.next.next
+        # return dummy.next
+        return head
